67a511b91fdcd8895c8688ae96d3a9137b8fe423
catnip <dr4kfire@proton.me>
2026-02-20 20:07:14 +0100

    refactor: changed project structure + new engine.h

---
+++ src/game/game_main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by catnip on 2/20/26.
+//
+
+#include <string>
+
+#include "raygui.h"
+#include "core/engine.h"
+
+
+int count = 0;
+
+
+void draw_game(double);
+void game_main(Engine *engine) {
+    engine->addDrawFunc(&draw_game);
+}
+
+void draw_game(double deltaTime) {
+    if (GuiButton((Rectangle){ 24, 24, 120, 30 }, std::to_string(count).c_str())) count++;
+}
