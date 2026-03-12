c91529972ebdce90238b44456a8e98893a44057a
orl0pl <93262125+orl0pl@users.noreply.github.com>
2026-03-12 19:13:07 +0100

    content: added lowkey entity placeholder

---
+++ src/game/src/mechanics/entities/lowkey/LowkeyEntity.h
@@ -0,0 +1,17 @@
+#ifndef REDPROJECT_LOWKEYENTITY_H
+#define REDPROJECT_LOWKEYENTITY_H
+
+#include "../entity.h"
+
+
+class LowkeyEntity: Entity {
+public:
+    
+
+private:
+    
+
+};
+
+
+#endif //REDPROJECT_LOWKEYENTITY_H
