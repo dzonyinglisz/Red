d48ed87535a0e78710cf1f1e0112ba2101fbbaf9
orl1pl <160421215+orl1pl@users.noreply.github.com>
2026-05-07 12:13:05 +0200

    game: added TimeKeeper

--- src/game/src/user-interface/main_view.h
+++ src/game/src/user-interface/main_view.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <raylib.h>
 #include <raygui.h>
+#include "game/src/mechanics/time.h"
 
 enum class UiState
 {
@@ -21,6 +22,7 @@ class MainGameView
 {
 public:
     UiState current_state = UiState::OFFICE;
+    TimeKeeper *timeKeeper;
     int current_cam = 0;
     void loadTextures();
     Texture officeView;
