69b8b58d4a30b548a08810d953f5fec4321e4d42
catnip <147838547+dr4kfire@users.noreply.github.com>
2026-05-07 11:47:56 +0200

    Work: Fixed build-shell.nix

--- build-shell.nix
+++ build-shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
   mkShell {
     nativeBuildInputs = [cmake pkg-config];
-    buildInputs = [libX11];
+    buildInputs = [libX11 libxrandr libxinerama libxcursor libxi mesa libGL libGLU];
   }
